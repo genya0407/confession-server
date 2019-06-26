f93acbedfc12796530e89b71b806a9253b2de13f
Yusuke Sangenya <longinus.eva@gmail.com>
2019-06-26 17:54:53 +0900

    usecase test

---
+++ usecase/send_message_anonymous_test.go
@@ -0,0 +1,78 @@
+package usecase
+
+import (
+	"fmt"
+	"github.com/genya0407/confession-server/entity"
+	"github.com/google/uuid"
+	"testing"
+	"time"
+)
+
+/*
+	ChatID          ChatID
+	Account         Account
+	Anonymous       Anonymous
+	Messages        []Message
+	StartedAt       time.Time
+	FinishedAt      *time.Time
+	AccountSocket   Socket
+	AnonymousSocket Socket
+	StoreMessage    StoreMessage
+	FinishChat      FinishChat
+*/
+
+func mustNewUUID() uuid.UUID {
+	u, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+type mockSocket struct {
+	c chan entity.Message
+}
+
+func (ms *mockSocket) SendText(msg entity.Message) {
+	ms.c <- msg
+}
+
+func (ms *mockSocket) Close() {
+	close(ms.c)
+}
+
+func TestSendMessageAnonymousToAccount(t *testing.T) {
+	anon := AnonymousLoginInfoDTO{SessionToken: "aaaaa"}
+	expectedChatID := mustNewUUID()
+	c := make(chan entity.Message)
+	findChatAnonymous := func(cID entity.ChatID, anon entity.Anonymous) (entity.Chat, bool) {
+		if cID == expectedChatID {
+			return entity.Chat{
+				ChatID:        expectedChatID,
+				AccountSocket: &mockSocket{c: c},
+				Anonymous: entity.Anonymous{
+					Token: anon.Token,
+				},
+			}, true
+		}
+
+		return entity.Chat{}, false
+	}
+
+	msgText := "abcdef"
+	msg := MessageDTO{
+		MessageID:   mustNewUUID(),
+		Text:        msgText,
+		SentAt:      time.Now(),
+		ByAnonymous: true,
+	}
+
+	sendMessageAnonymousToAccount := GenerateSendMessageAnonymousToAccount(findChatAnonymous)
+	go func() {
+		sendMessageAnonymousToAccount(anon, expectedChatID, msg)
+	}()
+	sentMessage := <-c
+	if sentMessage.Text != msgText {
+		t.Error("Invalid message")
+	}
+}
